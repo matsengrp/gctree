15c33f5ee66453c975b4dabdffacd259a5c2ec96
Will Dumm <wrhdumm@gmail.com>
2021-10-22 09:57:04 -0700

    lint and format tests

--- tests/test_disambiguate.py
+++ tests/test_disambiguate.py
@@ -3,21 +3,25 @@ import random
 import ete3
 import gctree.phylip_parse as pps
 
-newick_tree1 = ("((((12[&&NHX:name=12:sequence=T])4[&&NHX:name=4:sequence=C],"
-                "(6[&&NHX:name=6:sequence=C],"
-                "7[&&NHX:name=7:sequence=A])5[&&NHX:name=5:sequence=M])"
-                "3[&&NHX:name=3:sequence=M],8[&&NHX:name=8:sequence=A],"
-                "(11[&&NHX:name=11:sequence=A],10[&&NHX:name=10:sequence=G])"
-                "9[&&NHX:name=9:sequence=R])2[&&NHX:name=2:sequence=R])"
-                "1[&&NHX:name=1:sequence=G];")
+newick_tree1 = (
+    "((((12[&&NHX:name=12:sequence=T])4[&&NHX:name=4:sequence=C],"
+    "(6[&&NHX:name=6:sequence=C],"
+    "7[&&NHX:name=7:sequence=A])5[&&NHX:name=5:sequence=M])"
+    "3[&&NHX:name=3:sequence=M],8[&&NHX:name=8:sequence=A],"
+    "(11[&&NHX:name=11:sequence=A],10[&&NHX:name=10:sequence=G])"
+    "9[&&NHX:name=9:sequence=R])2[&&NHX:name=2:sequence=R])"
+    "1[&&NHX:name=1:sequence=G];"
+)
 
-newick_tree2 = ("((((12[&&NHX:name=12:sequence=T])4[&&NHX:name=4:sequence=C],"
-                "(6[&&NHX:name=6:sequence=C],"
-                "7[&&NHX:name=7:sequence=A])5[&&NHX:name=5:sequence=?])"
-                "3[&&NHX:name=3:sequence=?],8[&&NHX:name=8:sequence=A],"
-                "(11[&&NHX:name=11:sequence=A],10[&&NHX:name=10:sequence=G])"
-                "9[&&NHX:name=9:sequence=?])2[&&NHX:name=2:sequence=?])"
-                "1[&&NHX:name=1:sequence=G];")
+newick_tree2 = (
+    "((((12[&&NHX:name=12:sequence=T])4[&&NHX:name=4:sequence=C],"
+    "(6[&&NHX:name=6:sequence=C],"
+    "7[&&NHX:name=7:sequence=A])5[&&NHX:name=5:sequence=?])"
+    "3[&&NHX:name=3:sequence=?],8[&&NHX:name=8:sequence=A],"
+    "(11[&&NHX:name=11:sequence=A],10[&&NHX:name=10:sequence=G])"
+    "9[&&NHX:name=9:sequence=?])2[&&NHX:name=2:sequence=?])"
+    "1[&&NHX:name=1:sequence=G];"
+)
 
 tree1 = ete3.TreeNode(newick=newick_tree1, format=1)
 for node in tree1.traverse():
@@ -33,7 +37,7 @@ def treeprint(tree: ete3.TreeNode):
     tree = tree.copy()
     for node in tree.traverse():
         node.name = node.sequence
-    return(tree.write(format=8))
+    return tree.write(format=8)
 
 
 def sample(tree, n=20, **kwargs):
@@ -45,100 +49,123 @@ def sample(tree, n=20, **kwargs):
         t = pps.disambiguate(t, random_state=random.getstate(), **kwargs)
         print(treeprint(t))
         newickset.add(treeprint(t))
-    return(newickset)
+    return newickset
 
 
 def test_full_ambiguity():
     newickset = sample(tree2)
-    correctset = {"((((T)C,(C,A)C)C,A,(A,G)G)G);",
-                  "((((T)C,(C,A)A)A,A,(A,G)A)A);",
-                  "((((T)C,(C,A)C)C,A,(A,G)A)A);"}
+    correctset = {
+        "((((T)C,(C,A)C)C,A,(A,G)G)G);",
+        "((((T)C,(C,A)A)A,A,(A,G)A)A);",
+        "((((T)C,(C,A)C)C,A,(A,G)A)A);",
+    }
     if not newickset == correctset:
         missing = correctset - newickset
         wrong = newickset - correctset
-        print(f"\nDisambiguate function is missing {missing}\n"
-              f"and came up with these incorrect trees: {wrong}")
+        print(
+            f"\nDisambiguate function is missing {missing}\n"
+            f"and came up with these incorrect trees: {wrong}"
+        )
         raise ValueError("Invalid Disambiguation")
 
 
 def test_restricted_ambiguity():
     newickset = sample(tree1)
-    correctset = {'((((TT)CC,(CC,AA)CC)CC,AA,(AA,GG)GG)GG);',
-                  '((((TT)CC,(CC,AA)AC)AC,AA,(AA,GG)AG)AG);',
-                  '((((TT)CC,(CC,AA)AC)AC,AA,(AA,GG)AA)AA);',
-                  '((((TT)CC,(CC,AA)CC)CC,AA,(AA,GG)AA)AA);',
-                  '((((TT)CC,(CC,AA)CA)CA,AA,(AA,GG)GA)GA);',
-                  '((((TT)CC,(CC,AA)CC)CC,AA,(AA,GG)GA)GA);',
-                  '((((TT)CC,(CC,AA)CC)CC,AA,(AA,GG)AG)AG);',
-                  '((((TT)CC,(CC,AA)AA)AA,AA,(AA,GG)AA)AA);',
-                  '((((TT)CC,(CC,AA)CA)CA,AA,(AA,GG)AA)AA);'}
+    correctset = {
+        "((((TT)CC,(CC,AA)CC)CC,AA,(AA,GG)GG)GG);",
+        "((((TT)CC,(CC,AA)AC)AC,AA,(AA,GG)AG)AG);",
+        "((((TT)CC,(CC,AA)AC)AC,AA,(AA,GG)AA)AA);",
+        "((((TT)CC,(CC,AA)CC)CC,AA,(AA,GG)AA)AA);",
+        "((((TT)CC,(CC,AA)CA)CA,AA,(AA,GG)GA)GA);",
+        "((((TT)CC,(CC,AA)CC)CC,AA,(AA,GG)GA)GA);",
+        "((((TT)CC,(CC,AA)CC)CC,AA,(AA,GG)AG)AG);",
+        "((((TT)CC,(CC,AA)AA)AA,AA,(AA,GG)AA)AA);",
+        "((((TT)CC,(CC,AA)CA)CA,AA,(AA,GG)AA)AA);",
+    }
     if not newickset == correctset:
         missing = correctset - newickset
         wrong = newickset - correctset
-        print(f"\nDisambiguate function is missing {missing}\n"
-              f"and came up with these incorrect trees: {wrong}")
+        print(
+            f"\nDisambiguate function is missing {missing}\n"
+            f"and came up with these incorrect trees: {wrong}"
+        )
         raise ValueError("Invalid Disambiguation")
 
 
 def test_restricted_ambiguity_widewindow_mutability():
-    mmodel = mutation_model.MutationModel(mutability_file="S5F/Mutability.csv", substitution_file="S5F/Substitution.csv")
-    newickset = sample(tree1, n=5, dist_func=utils.mutability_distance(mmodel), distance_dependence=2)
-    correctset = {'((((TT)CC,(CC,AA)CA)CA,AA,(AA,GG)GA)GA);'}
+    mmodel = mutation_model.MutationModel(
+        mutability_file="S5F/Mutability.csv", substitution_file="S5F/Substitution.csv"
+    )
+    newickset = sample(
+        tree1, n=5, dist_func=utils.mutability_distance(mmodel), distance_dependence=2
+    )
+    correctset = {"((((TT)CC,(CC,AA)CA)CA,AA,(AA,GG)GA)GA);"}
     if not newickset == correctset:
         missing = correctset - newickset
         wrong = newickset - correctset
-        print(f"\nDisambiguate function is missing {missing}\n"
-              f"and came up with these incorrect trees: {wrong}")
+        print(
+            f"\nDisambiguate function is missing {missing}\n"
+            f"and came up with these incorrect trees: {wrong}"
+        )
         raise ValueError("Invalid Disambiguation")
+
 
 def test_restricted_ambiguity_widewindow():
     newickset = sample(tree1, n=100, distance_dependence=-1)
-    correctset = {'((((TT)CC,(CC,AA)CC)CC,AA,(AA,GG)GG)GG);',
-                  '((((TT)CC,(CC,AA)AC)AC,AA,(AA,GG)AG)AG);',
-                  '((((TT)CC,(CC,AA)AC)AC,AA,(AA,GG)AA)AA);',
-                  '((((TT)CC,(CC,AA)CC)CC,AA,(AA,GG)AA)AA);',
-                  '((((TT)CC,(CC,AA)CA)CA,AA,(AA,GG)GA)GA);',
-                  '((((TT)CC,(CC,AA)CC)CC,AA,(AA,GG)GA)GA);',
-                  '((((TT)CC,(CC,AA)CC)CC,AA,(AA,GG)AG)AG);',
-                  '((((TT)CC,(CC,AA)AA)AA,AA,(AA,GG)AA)AA);',
-                  '((((TT)CC,(CC,AA)CA)CA,AA,(AA,GG)AA)AA);'}
+    correctset = {
+        "((((TT)CC,(CC,AA)CC)CC,AA,(AA,GG)GG)GG);",
+        "((((TT)CC,(CC,AA)AC)AC,AA,(AA,GG)AG)AG);",
+        "((((TT)CC,(CC,AA)AC)AC,AA,(AA,GG)AA)AA);",
+        "((((TT)CC,(CC,AA)CC)CC,AA,(AA,GG)AA)AA);",
+        "((((TT)CC,(CC,AA)CA)CA,AA,(AA,GG)GA)GA);",
+        "((((TT)CC,(CC,AA)CC)CC,AA,(AA,GG)GA)GA);",
+        "((((TT)CC,(CC,AA)CC)CC,AA,(AA,GG)AG)AG);",
+        "((((TT)CC,(CC,AA)AA)AA,AA,(AA,GG)AA)AA);",
+        "((((TT)CC,(CC,AA)CA)CA,AA,(AA,GG)AA)AA);",
+    }
     if not newickset == correctset:
         missing = correctset - newickset
         wrong = newickset - correctset
-        print(f"\nDisambiguate function is missing {missing}\n"
-              f"and came up with these incorrect trees: {wrong}")
+        print(
+            f"\nDisambiguate function is missing {missing}\n"
+            f"and came up with these incorrect trees: {wrong}"
+        )
         raise ValueError("Invalid Disambiguation")
+
 
 def test_sequence_disambiguate():
     sequences = ["GVNT", "?TCM"]
     lsts = [list(utils.disambiguations(sequence)) for sequence in sequences]
     correctsets = [
-        {"GAAT",
-         "GACT",
-         "GAGT",
-         "GATT",
-         "GCAT",
-         "GCCT",
-         "GCGT",
-         "GCTT",
-         "GGAT",
-         "GGCT",
-         "GGGT",
-         "GGTT"},
-        {"ATCA",
-         "ATCC",
-         "GTCA",
-         "GTCC",
-         "CTCA",
-         "CTCC",
-         "TTCA",
-         "TTCC",
-         "-TCA",
-         "-TCC"}
+        {
+            "GAAT",
+            "GACT",
+            "GAGT",
+            "GATT",
+            "GCAT",
+            "GCCT",
+            "GCGT",
+            "GCTT",
+            "GGAT",
+            "GGCT",
+            "GGGT",
+            "GGTT",
+        },
+        {
+            "ATCA",
+            "ATCC",
+            "GTCA",
+            "GTCC",
+            "CTCA",
+            "CTCC",
+            "TTCA",
+            "TTCC",
+            "-TCA",
+            "-TCC",
+        },
     ]
     for seq, lst, correctset in zip(sequences, lsts, correctsets):
         if not len(lst) == len(set(lst)):
             raise ValueError("Non-unique sequence disambiguation")
         if not set(lst) == correctset:
             raise ValueError(f"Incorrect disambiguation of sequence {seq}:\n{lst}")
-
