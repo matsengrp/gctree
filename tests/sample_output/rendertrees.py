744020f20716a2fbf513186d1d20e764b6dcd06d
Will Dumm <wrhdumm@gmail.com>
2021-11-16 10:49:36 -0800

    Add script to render original trees

---
+++ tests/sample_output/rendertrees.py
@@ -0,0 +1,11 @@
+import pickle
+
+with open('littleforest.p', 'rb') as fh:
+    forest = pickle.load(fh)
+
+forest.forest = tuple(sorted(forest.forest, key=lambda x: -x.ll(0.4961832069459249, 0.3658705215787903, build_cache=False)[0]))
+
+for index, ctree in enumerate(forest.forest):
+    ctree.render(outfile=f"gctree.out.{index}.tree.svg", idlabel=True)
+
+    
