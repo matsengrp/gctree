4a594f2c61ac74a9f7880a48059844e459265b02
Will Dumm <wrhdumm@gmail.com>
2021-11-16 10:44:14 -0800

    Formatted new tests

--- tests/test_isotype.py
+++ tests/test_isotype.py
@@ -2,11 +2,12 @@ import gctree.isotype as iso
 import ete3
 
 testtrees = [
-             ('((((A2)?,(G2,G2)?)?,G2,(A2,A2)?)?)M;', 3.0),
-             ('((((A2)?,(G2,G2)?)?,A2,(A2,A2)?)?)M;', 4.0),
-             ('((((A2)?,(G2,G3)?)?,G2,(A2,A2)?)?)M;', 5.0),
-             ('((((A2)?,(G2,A2)?)?,G2,(A2,A2)?)?)M;', 4.0),
-             ]
+    ("((((A2)?,(G2,G2)?)?,G2,(A2,A2)?)?)M;", 3.0),
+    ("((((A2)?,(G2,G2)?)?,A2,(A2,A2)?)?)M;", 4.0),
+    ("((((A2)?,(G2,G3)?)?,G2,(A2,A2)?)?)M;", 5.0),
+    ("((((A2)?,(G2,A2)?)?,G2,(A2,A2)?)?)M;", 4.0),
+]
+
 
 def test_isotype_disambiguate():
     newisotype = iso.IsotypeTemplate(["M", "G3", "A1", "G2", "G4", "E", "A2"]).new
@@ -15,4 +16,10 @@ def test_isotype_disambiguate():
         for node in tree.traverse():
             node.isotype = newisotype(node.name)
         iso.disambiguate_isotype(tree)
-        assert sum(iso.isotype_distance(node.up.isotype, node.isotype) for node in tree.iter_descendants()) == weight
+        assert (
+            sum(
+                iso.isotype_distance(node.up.isotype, node.isotype)
+                for node in tree.iter_descendants()
+            )
+            == weight
+        )
