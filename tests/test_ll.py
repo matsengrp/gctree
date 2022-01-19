889c016ce77fcc2da2ab73e431f816967cece3d5
Will Dumm <wrhdumm@gmail.com>
2022-01-18 18:05:32 -0800

    Add failing test for another underflow error

--- tests/test_ll.py
+++ tests/test_ll.py
@@ -1,14 +1,22 @@
 import gctree.branching_processes as bp
+import gctree.phylip_parse as pp
 import numpy as np
 from scipy.special import logsumexp, softmax
 from functools import lru_cache
 
+
+trees = pp.parse_outfile('tests/small_outfile', root='GL', abundance_file='tests/abundances.csv')
+forest = bp.CollapsedForest([bp.CollapsedTree(tree) for tree in trees])
 
 def test_ll_genotype_cache():
     p, q = 0.4, 0.6
     c_max, m_max = 10, 10
     bp.CollapsedTree._build_ll_genotype_cache(c_max, m_max, p, q)
 
+def test_forest_likelihoods():
+    # make sure ll doesn't have underflow
+    p, q = 0.4, 0.6
+    forest.ll(p, q, marginal=True)
 
 def validate_ll_genotype():
     c_max, m_max = 10, 10
