fix logsumexp underflow when 0 in b

tests/test_ll.py
@@ -5,18 +5,23 @@ from scipy.special import logsumexp, softmax
 from functools import lru_cache
 
 
-trees = pp.parse_outfile('tests/small_outfile', root='GL', abundance_file='tests/abundances.csv')
+trees = pp.parse_outfile(
+    "tests/small_outfile", root="GL", abundance_file="tests/abundances.csv"
+)
 forest = bp.CollapsedForest([bp.CollapsedTree(tree) for tree in trees])
+
 
 def test_ll_genotype_cache():
     p, q = 0.4, 0.6
     c_max, m_max = 10, 10
     bp.CollapsedTree._build_ll_genotype_cache(c_max, m_max, p, q)
 
+
 def test_forest_likelihoods():
     # make sure ll doesn't have underflow
     p, q = 0.4, 0.6
     forest.ll(p, q, marginal=True)
+
 
 def validate_ll_genotype():
     c_max, m_max = 10, 10