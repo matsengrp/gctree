7cefbc5c5176647f3fa834f5cf4e6262a8409d85
Will Dumm <wrhdumm@gmail.com>
2022-01-31 15:54:24 -0800

    update historydag dependency

--- setup.py
+++ setup.py
@@ -36,6 +36,6 @@ setuptools.setup(
         "scipy",
         "seaborn",
         "multiset",
-        "historydag @ https://github.com/matsengrp/historydag/archive/d7288db28204d8bda940c7e4b9e83dafdd4e69ab.zip#egg=historydag-0.1"
+        "historydag"
     ],
 )
