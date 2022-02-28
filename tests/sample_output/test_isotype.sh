43e23237a7d345adb659f099c30d6212102c7b5e
William DeWitt <wsdewitt@gmail.com>
2022-02-28 15:00:57 -0800

    rm tests/sample_output

--- tests/sample_output/test_isotype.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-ml libGLU
-ml Xvfb
-
-TMPDIR=/tmp xvfb-run -a \
-    isotype --parsimony_forest littleforest.p \
-    --inference_log gctree.inference.log --isotype_mapfile example.isotypemap \
-    --idmapfile example.idmap --out_directory isotypes
-
