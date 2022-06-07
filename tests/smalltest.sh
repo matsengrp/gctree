b1d8abfec6e751ffd129b1522c3132de4d15cf67
Will Dumm <wrhdumm@gmail.com>
2022-06-07 09:55:44 -0700

    fix abundance filepath

--- tests/smalltest.sh
+++ tests/smalltest.sh
@@ -3,7 +3,7 @@
 export QT_QPA_PLATFORM=offscreen
 export XDG_RUNTIME_DIR=/tmp/runtime-runner
 export MPLBACKEND=agg
-gctree infer tests/small_outfile example/abundances.csv --root GL --frame 1 --verbose --idlabel 
-gctree infer tests/small_outfile example/abundances.csv --root GL --frame 1 --verbose --idlabel --idmapfile tests/idmap.txt --isotype_mapfile tests/isotypemap.txt
-gctree infer tests/small_outfile example/abundances.csv --root GL --frame 1 --verbose --idlabel --mutability S5F/Mutability.csv --substitution S5F/Substitution.csv
-gctree infer tests/small_outfile example/abundances.csv --root GL --frame 1 --verbose --idlabel --idmapfile tests/idmap.txt --isotype_mapfile tests/isotypemap.txt --mutability S5F/Mutability.csv --substitution S5F/Substitution.csv
+gctree infer tests/small_outfile tests/abundances.csv --root GL --frame 1 --verbose --idlabel 
+gctree infer tests/small_outfile tests/abundances.csv --root GL --frame 1 --verbose --idlabel --idmapfile tests/idmap.txt --isotype_mapfile tests/isotypemap.txt
+gctree infer tests/small_outfile tests/abundances.csv --root GL --frame 1 --verbose --idlabel --mutability S5F/Mutability.csv --substitution S5F/Substitution.csv
+gctree infer tests/small_outfile tests/abundances.csv --root GL --frame 1 --verbose --idlabel --idmapfile tests/idmap.txt --isotype_mapfile tests/isotypemap.txt --mutability S5F/Mutability.csv --substitution S5F/Substitution.csv
