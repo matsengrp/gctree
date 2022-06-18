b9f62e19cddcbfd104a6d9bf89e1a10606358924
William DeWitt <wsdewitt@gmail.com>
2022-06-18 14:39:43 -0700

    handle smalltest output

--- tests/smalltest.sh
+++ tests/smalltest.sh
@@ -3,7 +3,8 @@
 export QT_QPA_PLATFORM=offscreen
 export XDG_RUNTIME_DIR=/tmp/runtime-runner
 export MPLBACKEND=agg
-gctree infer tests/small_outfile tests/abundances.csv --root GL --frame 1 --verbose --idlabel 
-gctree infer tests/small_outfile tests/abundances.csv --root GL --frame 1 --verbose --idlabel --idmapfile tests/idmap.txt --isotype_mapfile tests/isotypemap.txt
-gctree infer tests/small_outfile tests/abundances.csv --root GL --frame 1 --verbose --idlabel --mutability S5F/Mutability.csv --substitution S5F/Substitution.csv
-gctree infer tests/small_outfile tests/abundances.csv --root GL --frame 1 --verbose --idlabel --idmapfile tests/idmap.txt --isotype_mapfile tests/isotypemap.txt --mutability S5F/Mutability.csv --substitution S5F/Substitution.csv
+mkdir -p tests/smalltest_output
+gctree infer tests/small_outfile tests/abundances.csv --outbase tests/smalltest_output/gctree.infer  --root GL --frame 1 --verbose --idlabel
+gctree infer tests/small_outfile tests/abundances.csv --outbase tests/smalltest_output/gctree.infer --root GL --frame 1 --verbose --idlabel --idmapfile tests/idmap.txt --isotype_mapfile tests/isotypemap.txt
+gctree infer tests/small_outfile tests/abundances.csv --outbase tests/smalltest_output/gctree.infer --root GL --frame 1 --verbose --idlabel --mutability S5F/Mutability.csv --substitution S5F/Substitution.csv
+gctree infer tests/small_outfile tests/abundances.csv --outbase tests/smalltest_output/gctree.infer --root GL --frame 1 --verbose --idlabel --idmapfile tests/idmap.txt --isotype_mapfile tests/isotypemap.txt --mutability S5F/Mutability.csv --substitution S5F/Substitution.csv
