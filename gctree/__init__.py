f86596b5f3f00a14ddd0bfa0ad400c0173fd0ff3
Will Dumm <wrhdumm@gmail.com>
2024-01-22 14:04:09 -0800

    format lint and test

--- gctree/__init__.py
+++ gctree/__init__.py
@@ -1,4 +1,5 @@
 from .branching_processes import CollapsedTree, CollapsedForest  # noqa
 
 from . import _version
-__version__ = _version.get_versions()['version']
+
+__version__ = _version.get_versions()["version"]
